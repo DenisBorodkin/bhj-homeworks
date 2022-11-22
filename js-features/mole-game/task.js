af3965b762611b881405e6488137593f491c2946
Denis Borodkin <den_borodkin@inbox.ru>
2022-11-22 19:17:51 +0300

    The third task is ready

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,34 @@
+
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+let counterDead = 0;
+let counterLost = 0;
+for (holeIndex = 1; holeIndex < 10; holeIndex++) {
+	getHole = index => document.getElementById(`hole${index}`);// выбор дыры по index
+	let hole = getHole(holeIndex);
+	hole.addEventListener('click', function () {
+		if (hole.classList.contains(`hole_has-mole`)) {
+			counterDead++;
+			dead.textContent = counterDead;
+			if (counterDead >= 10) {
+				alert("Вы победили!");
+				counterDead = 0;
+				counterLost = 0;
+				dead.textContent = counterDead;
+				lost.textContent = counterLost;
+			}
+		}
+		else {
+			counterLost++;
+			lost.textContent = counterLost;
+			if (counterLost >= 5) {
+				alert("Вы проиграли!");
+				counterDead = 0;
+				counterLost = 0;
+				dead.textContent = counterDead;
+				lost.textContent = counterLost;
+			}
+		}
+	});
+}
+

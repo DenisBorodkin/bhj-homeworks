86000bb772a7b0bcbe7ca8e3c62e3819d4f07137
Denis Borodkin <den_borodkin@inbox.ru>
2022-12-10 18:32:41 +0300

    Tne third task is ready_new

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,33 +2,33 @@
 const prevSlider = document.querySelector('.slider__arrow_prev');
 const nextSlider = document.querySelector('.slider__arrow_next');
 const slider = Array.from(document.getElementsByClassName('slider__item'));
-let slider_number = 0;
-const checkActiveClass = slider[slider_number].classList.contains('slider__item_active');
+let sliderNumber = 0;
+const checkActiveClass = slider[sliderNumber].classList.contains('slider__item_active');
 prevSlider.onclick = () => {
 	if (checkActiveClass) {
-		slider[slider_number].classList.remove('slider__item_active');
+		slider[sliderNumber].classList.remove('slider__item_active');
 	}
-	if (slider_number != 0) {
-		slider_number -= 1;
-		slider[slider_number].classList.add('slider__item_active');
+	if (sliderNumber != 0) {
+		sliderNumber -= 1;
+		slider[sliderNumber].classList.add('slider__item_active');
 	}
 	else {
-		slider_number = slider.length - 1;
-		slider[slider_number].classList.add('slider__item_active');
+		sliderNumber = slider.length - 1;
+		slider[sliderNumber].classList.add('slider__item_active');
 	}
 }
 nextSlider.onclick = () => {
 	if (checkActiveClass) {
-		slider[slider_number].classList.remove('slider__item_active');
+		slider[sliderNumber].classList.remove('slider__item_active');
 	}
 
-	if (slider_number < slider.length - 1) {
-		slider_number += 1;
-		slider[slider_number].classList.add('slider__item_active');
+	if (sliderNumber < slider.length - 1) {
+		sliderNumber += 1;
+		slider[sliderNumber].classList.add('slider__item_active');
 	}
 	else {
-		slider_number = 0;
-		slider[slider_number].classList.add('slider__item_active');
+		sliderNumber = 0;
+		slider[sliderNumber].classList.add('slider__item_active');
 	}
 }
 
